0569347815bcfddc0f91ef8ff7b6b4da221f0909
dopezinho <opugnero@gmail.com>
2023-01-03 12:33:54 -0300

    rikin hands

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const situations = [
     ['Hospital', ['Médico', 'Infermeiro', 'Cirurgião', 'Recepcionista', 'Hipocondriaco', 'O Doente', 'O Louco', 'A Madre', 'Anestesista', 'O Acidentado']],
     //['Mecanica', ],
     ['Metro', ['Artista', 'Segurança', 'Bilheteiro', 'O Que Pulou A Catraca', 'Executivo', 'Bêbado', 'Vendedor Ambulânte', 'Passageiro', 'Sem Máscara', 'O Atrasado']],
-    ['Padaria', ['Padeiro', 'Bêbado', 'Caixa', 'O Que Compra Coxa Creme', 'Cliente Virado', 'Chepeiro', 'Balconista', 'Eletricista', 'Vigilância Sanitária', 'Vizinho']], 
+    ['Padaria', ['Padeiro', 'Bêbado', 'Caixa', 'O Que Compra Coxa Creme', 'Cliente Virado', 'Chepeiro', 'Balconista', 'Eletricista', 'Vigilância Sanitária', 'Vizinho']],
     ['Parque', ['Skatista', 'Segurança Noturno', 'Vendedor de Água de Côco', 'Velha do Cooper', 'Boleiro', 'Jogador de Basquete', 'Guarda Civil Municipal', 'Casal Apaixonado', 'Jardineiro', 'Nóia']],
     ['Posto de Gasolina', ['Frentista', 'Bêbado', 'Vendedor da Conveniência', 'Mecânico', 'Gerente', 'Caminhoneiro', 'Cliente', 'Turista de Ônibos', 'Motoboy', 'Ciclista']],
-    ['Restaurante', ['Chefe', 'Bartender', 'Recepcionista', 'Caixa', 'Influênciadora', 'Vegeteriano', 'Cliente (Que Foge da Conta)', 'Inspetor Sanitârio', 'Casal Que Briga', 'Cliente Que Reclama' ]],
+    ['Restaurante', ['Chefe', 'Bartender', 'Recepcionista', 'Caixa', 'Influênciadora', 'Vegeteriano', 'Cliente (Que Foge da Conta)', 'Inspetor Sanitârio', 'Casal Que Briga', 'Cliente Que Reclama']],
     //['Rua', ],
     ['Super Mercado', ['Estoquista', 'Caixa', 'Jardineiro da Loja', 'Gerente', 'Cliente', 'Menores Comprando Bebida', 'Anunciante', 'Criança Perdida', 'Louco por Promoção', 'Empacotador']],
     //['Teatro', ],
@@ -28,45 +28,45 @@ const situations = [
 
 const startButtom = document.getElementById('start');
 
-startButtom.addEventListener('click', function() {
-    
+startButtom.addEventListener('click', function () {
+
     let nJogadores = document.getElementById('nJogadores').value;
     let nImpostores = document.getElementById('nImpostores').value;
-    personList = selectScenario(nJogadores, nImpostores);7
+    personList = selectScenario(nJogadores, nImpostores); 7
     let listaFinal = pushImpostor(personList, nImpostores);
     const main = document.getElementById('main');
     main.innerHTML = `<p>Número de Jogadores ${nJogadores}<p><br><p>Número de Impostores ${nImpostores}<p><br><button id = "next">Next</button>`
-    for (let i = 0; i <listaFinal[1].lenght; i++) {
+    for (let i = 0; i < listaFinal[1].lenght; i++) {
         nextPlayer(i);
         console.log(i);
-    }           
+    }
 
-    
-    
+
+
     console.log(listaFinal[1].length);
     return listaFinal
 });
 
 function nextPlayer(j) {
     let next = document.getElementById('next');
-    next.addEventListener('click', function() {
-        main.innerHTML = `<h3>Jogador ${i-1}<h3><br><p id = "secretIdentity" style = "display : none;">${listaFinal[1][i]}</p><br><button id = "next">Next</button>`;
+    next.addEventListener('click', function () {
+        main.innerHTML = `<h3>Jogador ${i - 1}<h3><br><p id = "secretIdentity" style = "display : none;">${listaFinal[1][i]}</p><br><button id = "next">Next</button>`;
         next = document.getElementById('next');
-        next.addEventListener('click', function() {
-            main.innerHTML = `<h3>Jogador ${i}<h3><br><p id = "secretIdentity">${listaFinal[1][i]}</p><button id = "next">Next</button>`;  
+        next.addEventListener('click', function () {
+            main.innerHTML = `<h3>Jogador ${i}<h3><br><p id = "secretIdentity">${listaFinal[1][i]}</p><button id = "next">Next</button>`;
         });
     });
 }
-    
+
 function selectScenario(nPessoas, nImpostores) {
-    let n = Math.floor(Math.random()*situations.length);
+    let n = Math.floor(Math.random() * situations.length);
     let local = situations[n][0];
     let persons = situations[n][1];
-    let personList = [[],[]]
+    let personList = [[], []]
     personList[0].push(local)
     let i = 0
     while (i < (nPessoas - nImpostores)) {
-        let m = Math.floor(Math.random()*persons.length);
+        let m = Math.floor(Math.random() * persons.length);
         let person = persons[m];
         persons.splice(m, 1);
         personList[1].push(person);
@@ -79,10 +79,10 @@ function selectScenario(nPessoas, nImpostores) {
 function pushImpostor(list, nImpostores) {
     let i = 0;
     while (i < nImpostores) {
-        list[1].push('Charlatão'); 
+        list[1].push('Charlatão');
         const main = document.getElementById('main');
         main.innerHTML = '';
-        i = i + 1; 
+        i = i + 1;
     }
-    return list; 
+    return list;
 }
